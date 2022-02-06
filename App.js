bb0abccf4425677bec4948b9b0676e576e67a7e6
Fathima Naaz <73298173+fathimanaaz@users.noreply.github.com>
2022-02-06 13:57:02 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import BottomTabNavigator from './components/BottomTabNavigator';
+
+export default class App extends Component {
+  render(){
+    return <BottomTabNavigator/>
+  }
+  
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
